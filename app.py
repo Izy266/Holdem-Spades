d3917c4d96c57bed97072e532a66373b66b6d34b
Izy266 <islamelkattawy@gmail.com>
2024-01-03 19:36:01 -0500

    Added invite url top of lobby

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, redirect, request
 from flask_socketio import SocketIO, emit
+import secrets
 from texas_holdem import *
 from utils import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = secrets.token_hex(32)
 socketio = SocketIO(app)
 holdem = TexasHoldem()
 
@@ -23,7 +24,9 @@ def test_disconnect():
 @socketio.on('create_game')
 def create_game(data):
     game_id = data['game_id']
+    creator_id = data['creator_id']
     emit('game_created', {'game_id': game_id}, broadcast=True)
+    return creator_id
 
 @app.route('/create_game', methods=['POST'])
 def handle_create_game():
@@ -32,7 +35,9 @@ def handle_create_game():
     player_name = request.form['player_name']
     starting_balance = request.form['starting_balance']
     holdem.add_player(player_name, starting_balance)
-    return '/game/' + game_id + '/join'
+    players = holdem.players
+    join_url = request.host_url + 'game/' + game_id + '/join'
+    return render_template('lobby.html', join_url=join_url, players=players)
 
 @app.route('/add_player', methods=['POST'])
 def handle_add_player():
@@ -40,11 +45,20 @@ def handle_add_player():
     starting_balance = request.form['starting_balance']
     holdem.add_player(player_name, starting_balance)
     players = holdem.players
-    return render_template('game.html', players=players)
+    return render_template('lobby.html', players=players)
 
 @app.route('/game/<game_id>/join')
 def player(game_id):
     return render_template('player.html', game_id=game_id)
+
+# @app.route('/game/<game_id>')
+# def start_game(game_id):
+#     creator_id = request.args.get('creator_id')
+#     if creator_id == request.cookies.get('player_id'):
+#         # Start the game
+#         return redirect(url_for('game', game_id=game_id))
+#     else:
+#         return 'You are not authorized to start this game.'
 
 if __name__ == '__main__':
     socketio.run(app)

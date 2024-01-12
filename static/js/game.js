cd6e9fc0f4f45958b3dcf4744dc92c2bff36a137
Izy266 <islamelkattawy@gmail.com>
2024-01-12 11:09:32 -0500

    Updated to div cards rather than svg, updated UI elements

--- static/js/game.js
+++ static/js/game.js
@@ -12,22 +12,37 @@ function getCookie(name) {
     return null;
 }
 
-function getCardImgUrl(card) {
-    const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-    let suite = suits[card[1]];
+function makeCard(card) {
+    const cardFront = document.createElement('div');
+    const suiteContainer = document.createElement('div');
+    const cardSuite = document.createElement('p');
+    const cardRank = document.createElement('p');
+    const suiteConv = ['♣', '♥', '♠', '♦'];
+    const rankConv = ['J', 'Q', 'K', 'A'];
+
+    suiteContainer.appendChild(cardSuite);
+    cardFront.appendChild(cardRank);
+    cardFront.appendChild(suiteContainer);
+
+    let suite = suiteConv[card[1]];
     let rank = card[0];
 
-    if (rank == 11) {
-        rank = 'jack';
-    } else if (rank == 12) {
-        rank = 'queen';
-    } else if (rank == 13) {
-        rank = 'king';
-    } else if (rank == 14) {
-        rank = 'ace';
+    if (rank > 10) {
+        rank = rankConv[rank - 11];
+    } else {
+        rank = rank.toString();
     }
 
-    return `https://tekeye.uk/playing_cards/images/svg_playing_cards/fronts/${suite}_${rank}.svg`;
+    cardRank.innerHTML = rank;
+    cardSuite.innerHTML = suite;
+
+    if (['♥', '♦'].includes(suite)) {
+        cardFront.className = "card card_front card_red";
+    } else {
+        cardFront.className = "card card_front";
+    }
+
+    return cardFront;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -37,18 +52,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const cbf = document.getElementById("cbf");
     const pot = document.getElementById("pot");
     const cards = document.getElementById("cards");
-    const playerList = document.getElementById('playerList');
+    const playerList = document.getElementById('player_list');
 
     socket.on('connect', () => {
         socket.emit('join', { gameId: gameId, playerId: playerId });
         socket.emit('playerAction', { gameId: gameId, playerId: playerId, action: 'none' });
-        socket.emit('getPlayerHand', { gameId: gameId, playerId: playerId });
     });
 
     socket.on('player_list', function (data) {
         const players = JSON.parse(data);
+        const playerExists = players.some(player => player.id === playerId);
 
-        players.forEach(function (player) {
+        if (!playerExists) {
+            window.location.href = '/join/' + gameId;
+        }
+
+        players.forEach(player => {
             let playerInfo = document.getElementById(player.id);
 
             if (playerInfo === null) {
@@ -60,47 +79,109 @@ document.addEventListener('DOMContentLoaded', () => {
                 const playerHand = document.createElement('div');
 
                 for (let i = 0; i < 2; i++) {
-                    let cardBack = document.createElement('img');
-                    cardBack.src = "https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/blue2.svg";
+                    let cardBack = document.createElement('div');
+                    let cardLogo = document.createElement('img');
+                    cardLogo.src = "../static/img/logo4.svg";
+                    cardBack.appendChild(cardLogo);
+                    cardBack.setAttribute('class', 'card card_back');
                     playerHand.appendChild(cardBack);
                 }
 
                 playerName.innerHTML = player.name;
-                playerBalance.className = "playerBalance";
-                playerName.className = "playerName";
-                playerHand.className = "playerHand";
+            
+                playerInfo.setAttribute('class', 'player_info')
+                playerBalance.setAttribute("class", "player_balance value_container");
+                playerName.setAttribute("class", "player_name");
+                playerHand.setAttribute("class", "player_hand");
 
-                playerInfo.appendChild(playerBalance);
                 playerInfo.appendChild(playerName);
                 playerInfo.appendChild(playerHand);
+                playerInfo.appendChild(playerBalance);
                 playerList.appendChild(playerInfo);
             }
 
-            playerBalance = playerInfo.querySelector('.playerBalance')
-            playerBalance.innerHTML = `$${player.balance}`
+            if (player.current) {
+                playerInfo.style.border = "1px solid rgb(0, 0, 0, 0.1)";
+                playerInfo.style.backgroundColor = "rgb(0, 100, 0, 0.2)"; 
+                playerInfo.style.boxShadow = "rgba(100, 0, 0, 0.6) 0px 7px 29px 0px";
+            } else {
+                playerInfo.style.border = ""; 
+                playerInfo.style.backgroundColor = ""; 
+                playerInfo.style.boxShadow = "";
+            }
+            console.log(player.creator)
+            const playerBalance = playerInfo.querySelector('.player_balance')
+            playerBalance.innerHTML = `Balance: $${player.balance}`
         });
     });
 
     socket.on(`player_hand`, function (data) {
         const playerInfo = document.getElementById(playerId);
-        const playerHand = playerInfo.querySelector('.playerHand');
+        const playerHand = playerInfo.querySelector('.player_hand');
         playerHand.innerHTML = '';
         data.cards.forEach(card => {
-            const cardFront = document.createElement('img');
-            cardFront.src = getCardImgUrl(card);
+            const cardFront = makeCard(card);
             playerHand.appendChild(cardFront);
         });
     });
 
     socket.on('game_info', function (data) {
+        let delay = 0;
         cbf.innerHTML = "";
         cards.innerHTML = "";
         pot.innerHTML = `Pot: ${data.pot}`;
 
+        if (data.live) {
+            socket.emit('getPlayerHand', { gameId: gameId, playerId: playerId });
+        } else if (playerId == data.creator_id) {
+            const startContainer = document.getElementById("start_container");
+            const startButton = document.createElement("button");
+            startContainer.innerHTML = "";
+            startButton.setAttribute("id", "start_game_button");
+            startButton.setAttribute("class", "common_button");
+            startButton.innerHTML = "Start Game";
+            startContainer.appendChild(startButton);
+            startButton.addEventListener("click", () => {
+                socket.emit('handStart', { gameId: gameId });
+                startContainer.removeChild(startButton);
+                socket.emit('playerAction', { gameId: gameId, playerId: playerId, action: 'none' });
+                players.forEach(p => {
+                    socket.emit('getPlayerHand', { gameId: gameId, playerId: p.id });
+                });
+            });
+        }
+
         data.cards.forEach(card => {
-            const imgElement = document.createElement('img');
-            imgElement.src = getCardImgUrl(card);
-            cards.appendChild(imgElement);
+            const tableCard = makeCard(card);
+            const flipCard = document.createElement('div');
+            const flipCardInner = document.createElement('div');
+            const flipCardFront = document.createElement('div');
+            const flipCardBack = document.createElement('div');
+            const cardBack = document.createElement('div');
+            const cardLogo = document.createElement('img');
+
+            cardBack.setAttribute('class', 'card card_back');
+            flipCard.setAttribute('class', 'flip-card');
+            flipCardInner.setAttribute('class', 'flip-card-inner');
+            flipCardFront.setAttribute('class', 'flip-card-front');
+            flipCardBack.setAttribute('class', 'flip-card-back');
+
+            flipCard.appendChild(flipCardInner);
+            flipCardInner.appendChild(flipCardFront);
+            flipCardInner.appendChild(flipCardBack);
+            flipCardFront.appendChild(cardBack);
+            flipCardBack.appendChild(tableCard);
+            cardBack.appendChild(cardLogo);
+            cards.appendChild(flipCard);
+            
+            delay += 300;
+
+            setTimeout(function () {
+                flipCard.classList.add('flip');
+            }, delay); 
+
+            cardLogo.src = "../static/img/logo4.svg";
+
         });
 
         if (data.cur_player_id === playerId) {
